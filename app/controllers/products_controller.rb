a19756ccaf6cb8f9911664287d8c3361fedbaf15
Dan Charlesworth <dcharlesworth@me.com>
2023-08-11 17:15:43 +1000

    Product management progress

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,74 @@
+class ProductsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_product, only: %i[ show edit update destroy ]
+
+  # GET /products
+  def index
+    @products = Product.all
+  end
+
+  # GET /products/1
+  def show
+  end
+
+  # GET /products/new
+  def new
+    @product = Product.new
+  end
+
+  # GET /products/1/edit
+  def edit
+  end
+
+  # POST /products
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.append(
+            'messages', partial: 'shared/errors',
+            locals: { errors: @product.errors }
+          ), status: :unprocessable_entity
+        end
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /products/1
+  def update
+    respond_to do |format|
+      if @product.update(product_params)
+        format.html { redirect_to product_url(@product), notice: "Product was successfully updated." }
+        format.json { render :show, status: :ok, location: @product }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @product.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /products/1
+  def destroy
+    @product.destroy
+
+    respond_to do |format|
+      format.html { redirect_to products_url, notice: "Product was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    def product_params
+      params.require(:product).permit(:title, :price, :unit, :description, :available)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :users, except: [:show]
 
   resources :customers
+  resources :products
 
   # Defines the root path route ("/")
   # root "articles#index"
