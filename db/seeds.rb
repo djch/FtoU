cf12853d3709b8d6df73978966f4a1db642ea2de
Dan Charlesworth <dcharlesworth@me.com>
2023-08-16 12:42:28 +1000

    Update seeds.rb
    Now includes Products and creates many more Customer records

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Customers
+# -----------------------------------------------------------------------------
 
 # Sample towns and postcodes from the Greater Hobart area of Tasmania
 towns_and_postcodes = [
@@ -13,22 +8,39 @@ towns_and_postcodes = [
   { town: "Kingston", postcode: "7050" },
   { town: "Claremont", postcode: "7011" },
   { town: "Sandy Bay", postcode: "7005" },
+  { town: "Bellerive", postcode: "7018" },
+  { town: "Moonah", postcode: "7009" },
+  { town: "Rosny Park", postcode: "7018" },
+  { town: "Battery Point", postcode: "7004" },
+  { town: "Blackmans Bay", postcode: "7052" },
+  { town: "Lindisfarne", postcode: "7015" },
+  { town: "Howrah", postcode: "7018" },
+  { town: "Taroona", postcode: "7053" },
+  { town: "South Hobart", postcode: "7004" },
+  { town: "Mount Stuart", postcode: "7000" },
+  { town: "West Hobart", postcode: "7000" },
+  { town: "North Hobart", postcode: "7000" },
+  { town: "Lenah Valley", postcode: "7008" },
+  { town: "New Town", postcode: "7008" },
+  { town: "Huonville", postcode: "7109" },
+  { town: "Geeveston", postcode: "7116" }
 ]
 
-# Randomly generated sample names
-names = [
-  "John Smith", "Jane Johnson", "Robert Brown", "Emily Davis", "William Taylor",
-  "Mary Wilson", "James White", "Patricia Harris", "Charles Clark", "Linda Lewis",
-]
+# Iterate through a number of times to create customers
+50.times do
+  first_name = Faker::Name.first_name
+  last_name = Faker::Name.last_name
 
-# Iterate through the towns and names to create customers
-names.each_with_index do |name, index|
+  # Check if customer already exists
+  next if Customer.exists?(first_name: first_name, last_name: last_name)
+
   town_and_postcode = towns_and_postcodes.sample
 
   Customer.create!(
-    name: name,
-    phone: "04#{Faker::Number.number(digits: 8)}", # Random mobile number with Australian prefix
-    email: Faker::Internet.email(name: name.downcase.gsub(" ", "_")),
+    first_name: first_name,
+    last_name: last_name,
+    phone: "04#{Faker::Number.number(digits: 8)}",
+    email: Faker::Internet.email(name: "#{first_name}_#{last_name}".downcase),
     street_address: "#{Faker::Number.number(digits: 3)} #{Faker::Address.street_name} St",
     town: town_and_postcode[:town],
     state: "TAS",
@@ -36,4 +48,59 @@ names.each_with_index do |name, index|
     country: "Australia",
     delivery_notes: "Leave at the front door"
   )
+end
+
+# Products
+# -----------------------------------------------------------------------------
+
+products_data = [
+  {
+    title: "10 to 12 Inch Split Firewood",
+    price: 130,
+    unit: "bucket",
+    available: true,
+    description: "<div>Ideal for smaller or older fireboxes</div>"
+  },
+  {
+    title: "12 to 15 Inch Split Firewood",
+    price: 125,
+    unit: "bucket",
+    available: true,
+    description: "<div>Most popular for larger fireplaces</div>"
+  },
+  {
+    title: "Bagged Firewood",
+    price: 120,
+    unit: "bag",
+    available: true,
+    description: "<div>10KG bag of dried hardwood</div>"
+  },
+  {
+    title: "Bagged Kindling",
+    price: 120,
+    unit: "bag",
+    available: true,
+    description: "<div>10KG bag of dried kindling<br>Lights quickly and easily to get your fire started</div>"
+  },
+  {
+    title: "Bagged Sawdust",
+    price: 120,
+    unit: "bag",
+    available: true,
+    description: "<div>10KG bag of dried sawdust<br>Lights quickly and easily to get your fire started</div>"
+  }
+]
+
+# Iterate through the products data and create products
+products_data.each do |product_data|
+  next if Product.exists?(title: product_data[:title])
+
+  product = Product.create!(
+    title: product_data[:title],
+    price: product_data[:price],
+    unit: product_data[:unit],
+    available: product_data[:available]
+  )
+  product.description = product_data[:description] # Assigning rich text
+  product.save!
 end
