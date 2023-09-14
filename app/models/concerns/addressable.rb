805efe700abd4b8ec8306cd4090c03db9f25daf8
Dan Charlesworth <dcharlesworth@me.com>
2023-09-14 15:30:37 +1000

    Validate street_address from places autocomplete

--- app/models/concerns/addressable.rb
+++ app/models/concerns/addressable.rb
@@ -7,5 +7,16 @@ module Addressable
     validates :state, presence: true
     validates :postcode, presence: true
     validates :country, presence: true
+
+    validate :street_address_contains_number
+
+    private
+
+      def street_address_contains_number
+        unless street_address =~ /\d/
+          # Sometimes Google Places lets you choose made up addresses, as long as the street name exists
+          errors.add(:street_address, "we couldn't validate that delivery address")
+        end
+      end
   end
 end
