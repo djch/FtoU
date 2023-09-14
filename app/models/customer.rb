0347ad99e9202f4bee2daeb4f5f5500d048fb156
Dan Charlesworth <dcharlesworth@me.com>
2023-09-15 09:47:13 +1000

    Update customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   include Addressable
   include PgSearch::Model
 
-  pg_search_scope :search_by_name, against: [:first_name, :last_name]
+  pg_search_scope :search_by_name, against: [:first_name, :last_name], using: { tsearch: { prefix: true } }
   has_person_name
 
   has_many :orders
