c5d21682cf97435d013e3e0d47e5a05a71947745
Dan Charlesworth <dcharlesworth@me.com>
2023-10-04 12:43:15 +1100

    Fix some delivery query issues with timezones

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -2,20 +2,29 @@ class DeliveriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @selected_date = params[:date]&.to_date || Date.today
+    @selected_date = params[:date]&.to_date || Date.current
 
     # Determine the displayed month and year
     @year = params[:year]&.to_i || @selected_date.year
     @month = params[:month]&.to_i || @selected_date.month
     @date = Date.new(@year, @month, 1)
 
+    start_time = @selected_date.beginning_of_day
+    end_time = @selected_date.end_of_day
 
     if params[:date]
       # Query the Order model based on the delivery_date field
-      @orders = Order.includes(:order_items, :customer).where(delivery_date: @selected_date)
+      @orders = Order.includes(:order_items, :customer)
+                     .where(delivery_date: start_time..end_time)
+                     .where.not(status: ['cancelled', 'pending'])
+                     .order(:delivery_date)
     else
       # Default to showing today's orders if no date parameter is provided
-      @orders = Order.includes(:order_items, :customer).where(delivery_date: Date.today)
+      @orders = Order.includes(:order_items, :customer)
+                     .where(delivery_date: Date.current.beginning_of_day..Date.current.end_of_day)
+                     .where.not(status: ['cancelled', 'pending'])
+                     .order(:delivery_date)
     end
   end
+
 end
