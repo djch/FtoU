a79c0c5fce89c9fa566a5e7cbe0da2971209f122
Dan Charlesworth <dcharlesworth@me.com>
2023-10-04 11:09:25 +1100

    Fix calendar picker prev/next month behaviour

--- app/controllers/deliveries_controller.rb
+++ app/controllers/deliveries_controller.rb
@@ -2,10 +2,13 @@ class DeliveriesController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @year = params[:year]&.to_i || Time.now.year
-    @month = params[:month]&.to_i || Time.now.month
-    @date = Date.new(@year, @month, 1)
     @selected_date = params[:date]&.to_date || Date.today
+
+    # Determine the displayed month and year
+    @year = params[:year]&.to_i || @selected_date.year
+    @month = params[:month]&.to_i || @selected_date.month
+    @date = Date.new(@year, @month, 1)
+
 
     if params[:date]
       # Query the Order model based on the delivery_date field
