8335adc7901d8af7192bd67eed1782668a175e52
Dan Charlesworth <dcharlesworth@me.com>
2023-11-17 09:50:05 +1100

    Remove unnecessary console.log

--- app/javascript/controllers/scroll_to_top_controller.js
+++ app/javascript/controllers/scroll_to_top_controller.js
@@ -2,10 +2,6 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  connect() {
-    console.log("ScrollToTop controller connected!");
-  }
-
   jump() {
     window.scrollTo(0, 0);
   }
